3df4bd03d89b3a7e5ecf2ecd8695418bc62e969b
CCesarFreitas <ccesar.caio.bsb@gmail.com>
2026-08-09 01:44:01 -0300

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # --- CONFIGURAÇÃO DA PÁGINA ---
 st.set_page_config(
-    page_title="Winning Wars - Competição Mensal", page_icon="⚔️", layout="wide"
+    page_title="Winning Wars APP", page_icon="⚔️", layout="wide"
 )
 
 
