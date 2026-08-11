ad2680240fe6dcb9bdd52e4b5d445f759f2f799d
CCesarFreitas <ccesar.caio.bsb@gmail.com>
2026-08-11 17:20:48 -0300

    app.py

--- app.py
+++ app.py
@@ -2233,7 +2233,7 @@ else:
 
   # TÍTULO PRINCIPAL
   st.markdown(
-      "<h1 class='main-title'>⚔️ Winning Wars APP</h1>",
+      "<h1 class='main-title'>⚔️ Winning Wars APP ⚔️</h1>",
       unsafe_allow_html=True,
   )
   st.markdown(
