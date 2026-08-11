f0ce413b18a7941f98e411a815e0f79e0a9230b0
CCesarFreitas <ccesar.caio.bsb@gmail.com>
2026-08-11 16:59:16 -0300

    app.py

--- app.py
+++ app.py
@@ -1039,7 +1039,7 @@ with col_nav:
   with st.container(key="top_nav_menu"):
     b1, b2, b3 = st.columns(3)
     with b1:
-      if st.button("⚔️ ENTRAR NOS LAYOUTS DE GUERRA", use_container_width=True):
+      if st.button("⚔️ LAYOUTS PARA GUERRA", use_container_width=True):
         st.session_state["pagina_atual"] = "layouts_guerra"
         st.rerun()
       st.markdown(
@@ -1047,7 +1047,7 @@ with col_nav:
           unsafe_allow_html=True,
       )
     with b2:
-      if st.button("🏆 EXPLORAR LAYOUTS RANKEADOS", use_container_width=True):
+      if st.button("🏆 LAYOUTS PARA RANKEADA", use_container_width=True):
         st.session_state["pagina_atual"] = "layouts_rankeada"
         st.rerun()
       st.markdown(
