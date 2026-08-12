6b4ecf1df3a4f0bff916a9c19c5ae3d1bafbd33f
CCesarFreitas <ccesar.caio.bsb@gmail.com>
2026-08-11 22:20:24 -0300

    app.py

--- app.py
+++ app.py
@@ -1448,7 +1448,7 @@ with col_nav:
           '<a'
           ' href="https://link.clashofclans.com/pt?action=OpenClanProfile&tag=2YPL9GU8Y"'
           ' target="_blank" rel="noopener noreferrer"'
-          ' class="top-nav-link nav-clan">🏰 VISITAR CLÃ VASTAYA ↗</a>',
+          ' class="top-nav-link nav-clan">🏰 CLÃ VASTAYA (guerra farm) ↗</a>',
           unsafe_allow_html=True,
       )
 
